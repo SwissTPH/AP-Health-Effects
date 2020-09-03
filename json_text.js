44c519daa0af4a37e43e5305ca8a52c8c70d05a5
Arnd Klotz <a.klotz@dr-carl.com>
2020-09-03 08:28:06 +0200

    Update 2.9.2020

---
+++ json_text.js
@@ -0,0 +1,255 @@
+// LUDOK Info-Texte
+//
+// Dr. Carl GmbH 2020
+
+arrPopups = [
+    {"id": 1,
+    "parent": "Aktuelle Luftbelastung",
+    "text_de": "<div class=\"i_header\">Aktuelle Luftbelastung</div>" +
+               "Die Luftqualität der Schweiz wird seit Mitte der 1980er-Jahre stetig besser. Für viele Schadstoffe und an vielen Orten in der Schweiz werden die meisten Immissionsgrenzwerte " +
+               "heute eingehalten. Häufig und teilweise erheblich überschritten werden hingegen die Immissionsgrenzwerte für Ozon (grossräumig). Trotz der Verbesserungen in den letzten Jahren " +
+               "werden die Immissionsgrenzwerte für Stickstoffdioxid NO<sub>2</sub> und Feinstaub PM10/PM2.5 noch teilweise überschritten, infolge der immer noch zu hohen Emissionen " +
+               "von Luftschadstoffen in der Schweiz und den Nachbarländern." +
+               "<br><br>" +
+               "Aktuelle Daten zur Luftqualität finden Sie auf der AirCHeck App oder " +
+               "<a href=\"https://www.bafu.admin.ch/bafu/de/home/themen/luft/zustand/daten/luftbelastung--aktuelle-daten.html\" target=\"_blank\">hier</a>." +
+               "<br><br>" +
+               "Weitere Informationen zu den aktuellen und historischen Messwerten finden Sie " +
+               "<a href=\"https://www.bafu.admin.ch/bafu/de/home/themen/luft/zustand/daten/luftbelastung--aktuelle-daten.html\" target=\"_blank\">hier</a>",
+    "text_en": "Aktuelle Luftbelastung EN",
+    "text_fr": "Aktuelle Luftbelastung FR",
+    "text_it": "Aktuelle Luftbelastung IT",
+    },
+    {"id": 2,
+    "parent": "Grenzwerte",
+    "text_de": "<div class=\"i_header\">Wirkschwellen und Grenzwerte</div>" +
+               "Die durch die WHO definierten Luftqualitätsrichtlinien definieren Werte, unterhalb welcher keine oder nur geringe Effekte auf die Gesundheit erwartet werden. " +
+               "Diese Werte werden auf Basis des vorhandenen wissenschaftlichen Wissens definiert. Die Immissionsgrenzwerte in der Schweiz stimmen in der Regel mit den Luftqualitätsrichtlinien " +
+               "der WHO überein. Ein Überblick zu Grenzwerten finden sie hier LINK ZU GRENZWERTEN" +
+               "<br><br>" +
+               "Die Belastungswerte von Schwefeldioxid SO<sub>2</sub> und Kohlenmonoxid CO in der Schweiz sind heutzutage so tief, dass keine schädlichen Wirkungen bei diesen Belastungen erwartet werden. " +
+               "Für Wirkungen von langfristiger Luftbelastung geht man heute davon aus, dass keine Wirkschwellen bestehen. Die WHO überarbeitet derzeit ihre Richtlinien für die Luftqualität. " +
+               "Die in der Schweiz gemessenen Belastungen kombiniert mit den kleinen Risiken bedeuten für das Individuum im Vergleich zu Lebensstilfaktoren wie Rauchen, Ernährung oder Bewegung " +
+               "eine geringe Gefahr" +
+               "<br><br>" +
+               "Weitere Informationen zu den aktuellen und historischen Messwerten finden Sie " +
+               "<a href=\"https://www.bafu.admin.ch/bafu/de/home/themen/luft/zustand/daten/luftbelastung--aktuelle-daten.html\" target=\"_blank\">hier</a>",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    },
+    {"id": 3,
+    "parent": "Quellen",
+    "text_de": "<div class=\"i_header\">Quellen der Luftbelastung</div>" +
+               "Die Quellen der Luftbelastung sind je nach Schadstoff verschieden. Zu den Hauptverursachern zählen Verkehr, Holzfeuerungen, Industrie, Gewerbe und Landwirtschaft." +
+               "<br><br>" +
+               "Weitere Informationen finden Sie " +
+               "<a href=\"https://www.bafu.admin.ch/bafu/de/home/themen/luft/inkuerze.html#1537492315\" target=\"_blank\">hier</a>",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    } ,
+    {"id": 4,
+    "parent": "Hintergrund",
+    "text_de": "<div class=\"i_header\">Hintergrund</div>" +
+               "Pro Tag atmen wir etwa 12'000 Liter Luft ein und aus. Für den Luftaustausch stehen einem Erwachsenen rund 140 Quadratkilometer Fläche zur Verfügung. " +
+               "Wenn die Luft durch Schadstoffe verunreinigt wird, hat das nachweislich negative Auswirkungen auf unsere Gesundheit. " +
+               "Wir haben für Sie die gesicherten und wahrscheinlichen Gesundheitsfolgen zusammengetragen und in einer interaktiven Grafik aufbereitet." +
+               "<br><br>" +
+               "Es werden daher Wirkungen unterschieden, die durch kurzfristig (über Stunden oder Tage) erhöhte Schadstoffbelastung und langfristig (über Monate und Jahre) erhöhte " +
+               "Schadstoffbelastung verursacht werden. Wobei die Effekte der langfristigen Belastung ausgeprägter sind." +
+               "<br><br>" +
+               "Das hier dargestellte Wissen basiert auf einer Vielzahl von epidemiologischen Studien. Für die Beurteilung der ursächlichen Wirkung der Luftschadstoffe werden zusätzlich " +
+               "experimentelle Studien an Mensch, Tier und Zellen herangezogen. Wir haben uns auf die gesicherten und wahrscheinlichen Effekte gemäss Einschätzungen der US-amerikanischen " +
+               "Umweltbehörde beschränkt. Diese zwei Stufen stellen die beiden höchsten der Kausalitätsskala aus insgesamt fünf Stufen dar." +
+               "<br><br>" +
+               "TEXT ursächlich 5 Stufen" +
+               "<br><br>" +
+               "Die beobachteten Effekte bzw. Risiken bewegen sich in der Regel im ein- bis unteren zweistelligen Prozentbereich. Zum Beispiel ist die Sterblichkeit bei einer langfristig " +
+               "erhöhten Belastung von 10 µg/m3 Feinstaub über Jahre mit einer erhöhten Gesamtsterblichkeit von 6% verbunden. Die Gefahr, welche durch Kombination des Risikos mit der " +
+               "tatsächlichen Belastung für das Individuum ausgeht, ist gegenüber anderen Lebensstilfaktoren wie Rauchen praktisch vernachlässigbar. " +
+               "Auf Ebene der Bevölkerung – also aus Public Health Sicht – ist eine solche Zunahme der Sterblichkeit aber bedeutend. Denn umgerechnet auf die Schweizer Bevölkerung bedeutet " +
+               "diese kleine Risikozunahme, dass pro Jahr xx REFERENZ Personen vorzeitig an den Folgen der Luftbelastung sterben." +
+               "<br><br>" +
+               "Hinzu kommt, dass die persönliche Krankengeschichte und die individuelle Empfindlichkeit eine grosse Rolle neben den gemessenen Schadstoffen in der Umgebung spielen.",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    } ,
+    {"id": 5,
+    "parent": "Was ich tun kann",
+    "text_de": "<div class=\"i_header\">Was kann ich für eine gesunde Luft und Umwelt tun?</div>" +
+               "Die Luftqualität ist ein Teil der Umwelt. Wer gerne etwas gegen die Luftbelastung tun möchte und somit der Gesundheit und der Umwelt etwas Gutes tut, kann sich gerne Anregungen " +
+               "<a href=\"https://www.vol.be.ch/vol/de/index/umwelt/luftreinhaltung/tipps_zur_luftreinhaltung.html\" target=\"_blank\">hier</a> " +
+               "und <a href=\"https://www.wwf.ch/de/nachhaltig-leben/10-besonders-wirksame-umwelt-tipps-fuer-privatpersonen\" target=\"_blank\">hier</a> holen.",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    } ,
+    {"id": 6,
+    "parent": "Referenzen",
+    "text_de": "<div class=\"i_header\">Referenzen</div>" +
+               "Als Datengrundlage für die Infografik dienten die aktuellen integrierten Wissenschaftsanalysen der US-amerikanischen Umweltbehörde zu den Schadstoffen " +
+               "Feinstaub PM10/PM2.5, Stickstoffdioxid NO<sub>2</sub>, Ozon O<sub>3</sub>, Schwefeldioxid SO<sub>2</sub> und Kohlenmonoxid CO: " + 
+               "<a href=\"https://www.epa.gov/isa\" target=\"_blank\">www.epa.gov/isa</a>" +
+               "<br><br>" +
+               "<ol>" +
+               "<li>U.S. EPA. Integrated Science Assessment (ISA) for Particulate (Final Report). U.S. Environmental Protection Agency, Washington, DC, EPA/600/R-19/188, 2019.</li>" +
+               "<li>U.S. EPA. Integrated Science Assessment (ISA) for Oxides of Nitrogen – Health Criteria (Final Report). U.S. Environmental Protection Agency, Washington, DC, EPA/600/R-15/068, 2016.</li>" +
+               "<li>U.S. EPA. Integrated Science Assessment (ISA) for Ozone and Related Photochemical Oxidants (Final Report). U.S. Environmental Protection Agency, Washington, DC, EPA/600/R-20/012, 2020.</li>" +
+               "<li>U.S. EPA. Integrated Science Assessment (ISA) for Sulfur Oxides – Health Criteria (Final Report). U.S. Environmental Protection Agency, Washington, DC, EPA/600/R-17/451, 2017.</li>" +
+               "<li>U.S. EPA. Integrated Science Assessment (ISA) for Carbon Monoxide (Final Report). U.S. Environmental Protection Agency, Washington, DC, EPA/600/R-09/019F, 2010.</li>" +
+               "<li>WHO AIR Q</li>" +
+               "</ol>",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    } ,
+    {"id": 7,
+    "parent": "Impressum",
+    "text_de": "<div class=\"i_header\">Impressum</div>" +
+               "Diese Grafik wurde durch die Dokumentationsstelle Luft und Gesundheit LUDOK (Link) am Schweizerischen Tropen- und Public Health Institut in Zusammenarbeit mit xyz realisiert." +
+               "<br><br>" +
+               "Der Inhalt entspricht dem Stand des Wissens von 2020." +
+               "<br><br>" +
+               "Unter Creative Commons xyz ist der Quellcode für die Grafik verfügbar. Verwendung und Anpassungen der Grafik sind nur nach Rücksprache mit dem Swiss TPH erlaubt.",
+    "text_en": "EN",
+    "text_fr": "FR",
+    "text_it": " IT",
+    }   
+  ];
+  
+  arrInfos = [
+    {"id": 1,
+    "parent": "Atemwege",
+    "text_de": "<div class=\"i_header\">Atemwege</div>Lunge, Luftröhre",
+    "text_en": "Atemwege EN",
+    "text_fr": "Atemwege FR",
+    "text_it": "Atemwege IT",
+    },
+    {"id": 2,
+    "parent": "Herz",
+    "text_de": "<div class=\"i_header\">Herz/Kreislauf</div>Herz, Blutkreislauf",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 3,
+    "parent": "Nervensystem",
+    "text_de": "<div class=\"i_header\">Nervensystem</div>Gehirn, Nervensystem",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 4,
+    "parent": "Stoffwechsel",
+    "text_de": "<div class=\"i_header\">Stoffwechsel/Immunsystem</div>Leber, Bauchspeicheldrüse, Immunabwehr",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 5,
+    "parent": "Sterblichkeit",
+    "text_de": "<div class=\"i_header\">Sterblichkeit</div>Sterbefälle, Anzahl der Todesfälle im Verhältnis zur Bevölkerung",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 6,
+    "parent": "Notfälle",
+    "text_de": "<div class=\"i_header\">Notfälle</div>Notfallkonsultationen in Spitälern oder beim Hausarzt, Spitaleintritte aufgrund von Notfällen",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 7,
+    "parent": "Organe_Effekte",
+    "text_de": "<div class=\"i_header\">Organe & Effekte</div>(zu Organe_Effekte gibt es noch keinen Tooltip Text)",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 7,
+    "parent": "causally",
+    "text_de": "<div class=\"i_header\">Ursächlich</div>" +
+               "Es hat sich gezeigt, dass der Schadstoff bei relevanten Expositionen zu Auswirkungen auf die Gesundheit oder das Wohlergehen führt, und zwar auf der Grundlage von Studien, die mehrere Beweislinien (Zell-, Tier-, Human- und epidemiologischen Studien) umfassen." +
+               "<br><br>" + 
+               "Zufallsergebnisse, Störfaktoren und andere Verzerrungen können mit angemessener Wahrscheinlichkeit ausgeschlossen werden." +
+               "<br><br>" + 
+               "(kausal)",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 7,
+    "parent": "likely",
+    "text_de": "<div class=\"i_header\">Wahrscheinlich ursächlich</div>" +
+               "Es gibt Studien, in denen die Ergebnisse nicht durch Zufallsergebnisse, Störfaktoren oder andere Verzerrungen erklärt werden, aber es bleiben Unsicherheiten in der Evidenz für Gesundheitseffekte insgesamt." +
+               "<br><br>" + 
+               "So ist z.B. der Einfluss von gleichzeitig auftretenden Schadstoffen schwer zu erfassen, oder die Evidenz in den verschiedenen wissenschaftlichen Disziplinen ist möglicherweise begrenzt oder inkonsistent." +
+               "<br><br>" + 
+               "(wahrscheinlich kausal)",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 8,
+    "parent": "Kurzzeit",
+    "text_de": "<div class=\"i_header\">Kurzzeit</div>" +
+               "Effekte durch kurzfristig erhöhte Belastungen über Stunden oder Tage",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 9,
+    "parent": "Langzeit",
+    "text_de": "<div class=\"i_header\">Langzeit</div>" +
+               "Effekte durch langfristig erhöhte Belastungen über längere Zeiträume (Monate oder Jahre)",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 10,
+    "parent": "Feinstaub",
+    "text_de": "<div class=\"i_header\">Feinstaub</div>" +
+               "Partikel kleiner als 10 bzw. 2.5 Mikrometer (PM10/PM2.5): Emissionen vor allem durch Verkehr oder Holzfeuerungen, (Wald-) Brände und sekundäre Bildung aus Vorläuferstoffen",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 11,
+    "parent": "Ozon",
+    "text_de": "<div class=\"i_header\">Ozon (O<sub>3</sub>)</div>" +
+               "Gebildet durch Sonneneinstrahlung aus Vorläuferstoffen wie Stickstoffdioxid und flüchtige organische Verbindungen",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 12,
+    "parent": "Stickstoffdioxid",
+    "text_de": "<div class=\"i_header\">Stickstoffdioxid (NO<sub>2</sub>)</div>" +
+               "Emissionen durch Verkehr und Holzfeuerungen",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 13,
+    "parent": "Schwefeldioxid",
+    "text_de": "<div class=\"i_header\">Schwefeldioxid (SO<sub>2</sub>)</div>" +
+               "Emissionen durch Verbrennung von schwefelhaltiger Kohle oder Erdöl",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+    {"id": 14,
+    "parent": "Kohlenmonoxid",
+    "text_de": "<div class=\"i_header\">Kohlenmonoxid (CO)</div>" +
+               "Emissionen durch unvollständige Verbrennungen von kohlenstoffhaltigen Brennstoffen",
+    "text_en": "",
+    "text_fr": "",
+    "text_it": "",
+    },
+  ];
+  
+   
+  
